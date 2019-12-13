d8cbf9f26bf2b57864cd0e939d34daf6cbe4881e
Shumo Chu <shumo@algorand.com>
2019-12-13 15:57:48 -0500

    wip teal sym execution

--- teal.rkt
+++ teal.rkt
@@ -15,35 +15,32 @@
 
 ; gloable parameters
 (struct global
-  (max_txn_fee min_balance max_txn_life) #:transparent)
+  (max_txn_fee min_balance max_txn_life zero_address group_size) #:transparent)
 
 ; eval parameters
 (struct eval_params
   (txn global args) #:transparent)
 
 ; define the context, TODO: add type guard to the arguments.
-(struct context (eval_params stack program pc next_pc err intc bytec) #:transparent)
+(struct context (eval_params stack program pc next_pc err) #:transparent)
 
 ; stack element.
-(struct stack-elmt (uint bytes) #:transparent)
+; type of stack element
+; 0 is uint
+; 1 is bytes
+(struct stack-elmt (value type) #:transparent)
 
 ; op-spec, roughly same as OpSpec in data/transaction/logic/eval.go.
 (struct op-spec (name op args returntype) #:transparent)
-
-; reflect the type of an stack element:
-; 0 is uint.
-; 1 is bytes.
-(define (elmt-type e)
-  (if (null? (stack-elmt-bytes e)) 1 0)) 
 
 (define uint64-max 18446744073709551615)
 
 (define ops
   (list
    (op-spec "err" op-err null "None")
-   (op-spec "sha256" op-sha256 '("Bytes") "Bytes")
+   ;(op-spec "sha256" op-sha256 '("Bytes") "Bytes")
    (op-spec "keccak256" op-keccak256 '("Bytes") "Bytes")
-   (op-spec "ed25519verify" op-ed25519verify '("Bytes" "Bytes" "Bytes") "Uint64")
+   ;(op-spec "ed25519verify" op-ed25519verify '("Bytes" "Bytes" "Bytes") "Uint64")
    (op-spec "+" op-plus '("Uint64" "Uint64") "Uint64")
    (op-spec "-" op-minus '("Uint64" "Uint64") "Uint64")
    (op-spec "/" op-div '("Uint64" "Uint64") "Uint64")
@@ -81,21 +78,33 @@
 
 ;; helper functions:
 
-; get the uint from the top of the stack.
+; get the top element of the stack and make sure it is uint
 (define (top-uint cxt)
-  (stack-elmt-uint (car (context-stack cxt))))
+  (let ([top-elmt (car (context-stack cxt))])
+    (if (equal? (stack-elmt-type top-elmt) 0)
+        (stack-elmt-value top-elmt)
+        (error 'type-error-expected-uint))))
 
-; get the uint from the second of the stack.
+; get the second element of the stack and make sure it is uint
 (define (second-uint cxt)
-  (stack-elmt-uint (car (cdr (context-stack cxt)))))
+  (let ([second-elmt (car (cdr (context-stack cxt)))])
+    (if (equal? (stack-elmt-type second-elmt) 0)
+        (stack-elmt-value second-elmt)
+        (error 'type-error-expected-uint))))
 
-; get the bytes from top of the stack.
+; get the top element of the stack and make sure it is bytes
 (define (top-bytes cxt)
-  (stack-elmt-bytes (car (context-stack cxt))))
+  (let ([top-elmt (car (context-stack cxt))])
+    (if (equal? (stack-elmt-type top-elmt) 1)
+        (stack-elmt-value top-elmt)
+        (error 'type-error-expected-uint))))
 
-; get the bytes from the second of the stack.
+; get the second element of the stack and make sure it is bytes
 (define (second-bytes cxt)
-  (stack-elmt-bytes (car (cdr (context-stack cxt)))))
+  (let ([second-elmt (car (cdr (context-stack cxt)))])
+    (if (equal? (stack-elmt-type second-elmt) 1)
+        (stack-elmt-value second-elmt)
+        (error 'type-error-expected-uint))))
 
 ; add an error message to the context.
 (define (add-err cxt msg)
@@ -120,14 +129,18 @@
            (context-bytec cxt)))
 
 ; push an int to a stack
-(define (push-int-stack cxt i)
-  (update-stack cxt (cons (stack-elmt i null) (context-stack cxt))))
+(define (push-int cxt i)
+  (update-stack cxt (cons (stack-elmt i 0) (context-stack cxt))))
 
 ; push bytes to a stack
-(define (push-bytes-stack cxt b)
-  (update-stack cxt (cons (stack-elmt 0 b) (context-stack cxt))))
+(define (push-bytes cxt b)
+  (update-stack cxt (cons (stack-elmt b 1) (context-stack cxt))))
 
 ;; opcode semantics:
+
+; int.
+(define (op-int cxt v)
+  (push-int cxt v))
 
 ; err.
 (define (op-err cxt)
@@ -147,7 +160,7 @@
   (let ([r (+ (second-uint cxt) (top-uint cxt))])
     (if (> r uint64-max)
         (add-err cxt "+ overflow")
-        (update-stack cxt (cons (stack-elmt r nil)
+        (update-stack cxt (cons (stack-elmt r 0)
                                 (cdr (cdr (context-stack cxt))))))))
 
 ; -
@@ -155,7 +168,7 @@
   (let ([r (- (second-uint cxt) (top-uint cxt))])
     (if (< r 0)
         (add-err cxt "- would result negative")
-        (update-stack cxt (cons (stack-elmt r nil)
+        (update-stack cxt (cons (stack-elmt r 0)
                                 (cdr (cdr (context-stack cxt))))))))
 
 ; /
@@ -163,7 +176,7 @@
   (if (= (top-uint cxt) 0)
       (add-err cxt "/ 0")
       (let ([r (/ (second-uint cxt) (top-uint cxt))])
-        (update-stack cxt (cons (stack-elmt r nil)
+        (update-stack cxt (cons (stack-elmt r 0)
                                 (cdr (cdr (context-stack cxt))))))))
 
 ; *
@@ -171,54 +184,54 @@
   (let ([r (* (top-uint cxt) (second-uint cxt))])
     (if (> r uint64-max)
         (add-err cxt "* overflow")
-        (update-stack cxt (cons (stack-elmt r nil)
+        (update-stack cxt (cons (stack-elmt r 0)
                                 (cdr (cdr (context-stack cxt))))))))
 
 ; compare two numbers
-(define (op-comp cxt op)
+(define (int-comp cxt op)
   (let ([r (if (op (second-uint cxt) (top-uint cxt)) 1 0)])
-    (update-stack cxt (cons (stack-elmt r nil)
+    (update-stack cxt (cons (stack-elmt r 0)
                             (cdr (cdr (context-stack cxt)))))))
 
 ; >
 (define (op-gt cxt)
-  (op-comp cxt >))
+  (int-comp cxt >))
 
 ; <
 (define (op-lt cxt)
-  (op-comp cxt <))
+  (int-comp cxt <))
 
 ; >=
 (define (op-ge cxt)
-  (op-comp cxt >=))
+  (int-comp cxt >=))
 
 ; <=
 (define (op-le cxt)
-  (op-comp cxt <=))
+  (int-comp cxt <=))
 
 ; and
 ; one caveat is that the two operands should never be negative
 (define (op-and cxt)
-  (op-comp cxt (lambda (a b) (or (= a 0) (= b 0))))) 
+  (int-comp cxt (lambda (a b) (or (= a 0) (= b 0))))) 
 
 ; or
 (define (op-or cxt)
-  (op-comp cxt (lambda (a b) (and (= a 0) (= b 0)))))        
+  (int-comp cxt (lambda (a b) (and (= a 0) (= b 0)))))        
 
 ; compare bytes
 (define (bytes-comp cxt op)
   (let ([r (if (op (second-bytes cxt) (top-bytes cxt)) 1 0)])
-    (update-stack cxt (cons (stack-elmt r nil)
+    (update-stack cxt (cons (stack-elmt r 1)
                             (cdr (cdr (context-stack cxt)))))))
 
 ; ==
 (define (op-eq cxt)
-  (let ([ta (elmt-type (car (context-stack cxt)))]
-        [tb (elmt-type (car (cdr (context-stack cxt))))])
+  (let ([ta (stack-elmt-type (car (context-stack cxt)))]
+        [tb (stack-elmt-type (car (cdr (context-stack cxt))))])
     (if (= ta tb)
         (if (= ta 1)
-            (bytes-comp cxt bveq)
-            (op-comp cxt =))
+            (bytes-comp cxt =)
+            (int-comp cxt =))
         (add-err "cannot compare values in different types"))))
 
 ; neq
@@ -228,41 +241,46 @@
     (if (= ta tb)
         (if (= ta 1)
             (bytes-comp cxt (lambda (a b) (not (bveq a b))))
-            (op-comp cxt (lambda (a b) (not (= a b)))))
+            (int-comp cxt (lambda (a b) (not (= a b)))))
         (add-err "cannot compare values in different types"))))
 
 ; !
 (define (op-not cxt)
   (let ([a (top-uint cxt)])
     (if (= a 0)
-        (update-stack cxt (cons (stack-elmt 1 null) (cdr (context-stack cxt)))))))
+        (update-stack cxt (cons (stack-elmt 1 0) (cdr (context-stack cxt)))))))
 
 ; txn
-(define (op-txn cxt)
-  (let ([txn (eval_params-txn (context-eval_params cxt))]
-        [idx (list-ref (context-program cxt) (+ (context-pc cxt) 1))])
+(define (op-txn cxt idx)
+  (let ([txn (eval_params-txn (context-eval_params cxt))])
     (match idx
-      [0 (push-bytes-stack (txn-sender txn))]
-      [1 (push-int-stack (txn-fee txn))]
-      [2 (push-int-stack (txn-first_valid txn))]
-      [3 (push-int-stack (txn-last_valid txn))]
-      [4 (push-bytes-stack (txn-note txn))]
-      [5 (push-bytes-stack (txn-receiver txn))]
-      [6 (push-int-stack (txn-amount txn))]
-      [7 (push-bytes-stack (txn-close_remainder_to txn))]
-      [8 (push-bytes-stack (txn-vote_pk txn))]
-      [9 (push-bytes-stack (txn-selection_pk txn))]
-      [10 (push-int-stack (txn-vote_first txn))]
-      [11 (push-int-stack (txn-vote_last txn))]
-      [12 (push-int-stack (txn-vote_key_dilution txn))]
+      [0 (push-bytes cxt (txn-sender txn))]
+      [1 (push-int cxt (txn-fee txn))]
+      [2 (push-int cxt (txn-first_valid txn))]
+      [3 (push-int cxt (txn-first_valid_time txn))]
+      [4 (push-int cxt (txn-last_valid txn))]
+      [5 (push-bytes cxt (txn-note txn))]
+      [6 (push-bytes cxt (txn-lease txn))]
+      [7 (push-bytes cxt (txn-receiver txn))]
+      [8 (push-int cxt (txn-amount txn))]
+      [9 (push-bytes cxt (txn-close_remainder_to txn))]
+      [10 (push-bytes cxt (txn-vote_pk txn))]
+      [11 (push-bytes cxt (txn-selection_pk txn))]
+      [12 (push-int cxt (txn-vote_first txn))]
+      [13 (push-int cxt (txn-vote_last txn))]
+      [14 (push-int cxt (txn-vote_key_dilution txn))]
+      [15 (push-bytes cxt (txn-type txn))]
+      [16 (push-int cxt (txn-type_enum txn))]
+      [17 (push-int cxt (txn-xfer_asset txn))]
+      [18 (push-int cxt (txn-asset_amount txn))]
+      [19 (push-bytes cxt (txn-asset_sender txn))]
+      [20 (push-bytes cxt (txn-asset_receiver txn))]
+      [21 (push-bytes cxt (txn-asset_close_to txn))]
+      [22 (push-int cxt (txn-group_index txn))]
+      [23 (push-bytes cxt (txn-tx_id txn))]
       [_ (add-err cxt (string-append "invalid txn field " (number->string idx)))]
       )))
 
-
-; len 
-(define (op-len cxt)
-  (let [bytes (top-bytes cxt)]
-    (update-stack cxt (cons (stack-elmt (length bytes) null) (rest (context-stack cxt))))))
   
 ; byte
 ; note: byte is not an actual opcode, the assembler will compile it to
