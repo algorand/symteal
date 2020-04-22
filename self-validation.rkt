67986020a44ed35d3dcab165f3364379b1c1f5dd
Shumo Chu <shumo.chu@gmail.com>
2020-04-22 12:09:15 -0400

    update self validation numbers

--- self-validation.rkt
+++ self-validation.rkt
@@ -1,6 +1,12 @@
 #lang rosette
 
-(require "ledger.rkt" "symbolic.rkt" "config.rkt" rackunit rackunit/text-ui rosette/lib/roseunit)
+(require "ledger.rkt" "symbolic.rkt" "config.rkt"
+         rackunit rackunit/text-ui rosette/lib/roseunit
+         rosette/solver/smt/boolector)
+
+(current-solver (boolector))
+
+(current-bitwidth 64)
 
 ; We verify several safety property of the implemented racket verifier itself:
 ; 1. After a transaction, the total amount of Algos in the system should be unchanged
@@ -21,28 +27,19 @@
 (define result-ledger-state
   (txn-group-eval sym-ledger-state (gen-sym-round) (list sym-txn) (gen-sym-global-params)))
 
-(define-syntax-rule (check-verify pred test)
-  (let ([sol (with-handlers ([exn:fail? (const (unsat))])
-               test)])
-    (check-true (pred sol) (format "not ~a for ~a: ~a" (quote pred) (quote test) sol))))
-
-; now check the property, expect unsat
-;(define ledger-safety-tests
-;  (test-suite+
-;   "ledger safety test"
 (ledger-precondition)
-(define sol (verify (assert (= (total-algos sym-ledger-state)
-                               (total-algos result-ledger-state)))))
 
-(print sol)
-(evaluate sym-txn sol)
-(evaluate sym-ledger-state sol)
+; verify algo balance invariant
+; cpu time: 56380 real time: 56727 gc time: 29262
+(time (verify (assert (= (total-algos sym-ledger-state)
+                   (total-algos result-ledger-state)))))
 
-;(define-symbolic asset integer?)
-;(assert (>= asset 0))
-;(assert (< asset asset-capacity))
-;(verify (assert (= (total-asset sym-ledger-state asset)
-;                   (total-asset result-ledger-state asset))))
-;   ))
+(define-symbolic asset integer?)
+(assert (>= asset 0))
+(assert (< asset asset-capacity))
 
-;(run-tests ledger-safety-tests)
+; verify asset balance invariant
+; cpu time: 692190 real time: 695380 gc time: 419965
+(time (verify (assert (= (total-asset sym-ledger-state asset)
+                         (total-asset result-ledger-state asset)))))
+
